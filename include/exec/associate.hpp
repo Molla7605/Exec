31df584958b7a1b7c27f2ad4320d7ba56bd5bf1e
Molla7605 <jyc760500@gmail.com>
2026-02-05 02:16:31 +0900

    Resolve conflict

---
+++ include/exec/associate.hpp
@@ -0,0 +1,9 @@
+#ifndef EXEC_ASSOCIATE_HPP
+#define EXEC_ASSOCIATE_HPP
+
+namespace exec {
+    struct associate_t {};
+    inline constexpr associate_t associate{};
+}
+
+#endif // !EXEC_ASSOCIATE_HPP
