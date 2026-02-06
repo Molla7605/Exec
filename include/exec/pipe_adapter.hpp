5a3b5fe615edd3296233594403e3b136212b139c
Molla7605 <jyc760500@gmail.com>
2026-02-07 03:45:32 +0900

    Remove noexcept from pipe operator

--- include/exec/pipe_adapter.hpp
+++ include/exec/pipe_adapter.hpp
@@ -12,7 +12,7 @@ namespace exec {
         [[no_unique_address]] CreatorT creator;
         [[no_unique_address]] std::tuple<ArgTs...> args;
 
-        [[nodiscard]] friend constexpr sender auto operator|(sender auto&& sndr, pipe_adapter&& self) noexcept {
+        [[nodiscard]] friend constexpr sender auto operator|(sender auto&& sndr, pipe_adapter&& self) {
             return std::apply([&]<typename... Ts>(Ts&&... args) constexpr {
                 return std::forward_like<decltype(self)>(self.creator(std::forward<decltype(sndr)>(sndr), std::forward<Ts>(args)...));
             }, std::forward_like<decltype(self)>(self.args));
