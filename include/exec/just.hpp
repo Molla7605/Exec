3f3a04b57bb391c5f544861dc71271ac6eba18c8
Molla7605 <jyc760500@gmail.com>
2026-02-20 04:44:51 +0900

    Refactor just_t to use basic_sender

--- include/exec/just.hpp
+++ include/exec/just.hpp
@@ -3,73 +3,67 @@
 
 #include "exec/completions.hpp"
 #include "exec/completion_signatures.hpp"
-#include "exec/operation_state.hpp"
-#include "exec/receiver.hpp"
-#include "exec/sender.hpp"
 
-#include <tuple>
+#include "exec/details/basic_sender.hpp"
+#include "exec/details/product_type.hpp"
+#include "exec/details/type_list.hpp"
+
 #include <type_traits>
 #include <utility>
 
 namespace exec {
-    template<typename TagT, typename ReceiverT, typename... ValueTs>
-    struct just_operation_state {
-        using operation_state_concept = operation_state_t;
+    template<typename CompletionT>
+    struct just_tag_t;
 
-        ReceiverT receiver;
-        [[no_unique_address]] std::tuple<ValueTs...> tuple;
-
-        constexpr void start() & noexcept {
-            std::apply(
-                [&]<typename... Ts>(Ts&&... values) noexcept {
-                    TagT{}(std::move(receiver), std::forward<Ts>(values)...);
-                },
-                std::move(tuple)
-            );
-        }
-    };
-
-    template<typename TagT, typename... ValueTs>
-    struct just_sender {
-        using sender_concept = sender_t;
-
-        using completion_signatures = completion_signatures<TagT(ValueTs...)>;
-
-        [[no_unique_address]] std::tuple<ValueTs...> tuple;
-
-        template<typename Self, receiver ReceiverT>
-        [[nodiscard]] constexpr auto connect(this Self&& self, ReceiverT&& rcvr) ->
-            just_operation_state<TagT, std::decay_t<ReceiverT>, ValueTs...>
-        {
-            return { std::forward<ReceiverT>(rcvr), std::forward_like<Self>(self.tuple) };
-        }
-    };
-
-    struct just_t {
+    template<typename CompletionT>
+    struct details::impls_for<just_tag_t<CompletionT>> : default_impls {
         template<typename... Ts>
-        [[nodiscard]] constexpr auto operator()(Ts&&... values) const ->
-            just_sender<set_value_t, std::decay_t<Ts>...>
-        {
-            return { { std::forward<Ts>(values)... } };
+        using signature_t = completion_signatures<CompletionT(Ts...)>;
+
+        static constexpr auto get_completion_signatures =
+            []<typename SenderT, typename EnvT>(const SenderT& sender, const EnvT&) noexcept {
+                return sender.apply([]<typename DataT>(auto&&, const DataT&, auto&&...) noexcept ->
+                           elements_of<DataT>::template apply<signature_t>
+                       {
+                           return {};
+                       });
+            };
+
+        static constexpr auto start =
+            []<typename StateT, typename ReceiverT>(StateT& state, ReceiverT& receiver, auto&&...) noexcept {
+                std::move(state).apply([&]<typename... Ts>(Ts&&... values) mutable noexcept {
+                    CompletionT{}(std::move(receiver), std::forward<Ts>(values)...);
+                });
+            };
+    };
+
+    template<>
+    struct just_tag_t<exec::set_value_t> {
+        template<typename... Ts>
+        [[nodiscard]] constexpr decltype(auto) operator()(Ts&&... values) const {
+            return details::make_sender(*this, details::product_type{ std::forward<Ts>(values)... });
         }
     };
+    using just_t = just_tag_t<exec::set_value_t>;
     inline constexpr just_t just{};
 
-    struct just_error_t {
+    template<>
+    struct just_tag_t<exec::set_error_t> {
         template<typename T>
-        [[nodiscard]] constexpr auto operator()(T&& value) const ->
-            just_sender<set_error_t, std::decay_t<T>>
-        {
-            return { std::forward<T>(value) };
+        [[nodiscard]] constexpr decltype(auto) operator()(T&& value) const {
+            return details::make_sender(*this, details::product_type{ std::forward<T>(value) });
         }
     };
+    using just_error_t = just_tag_t<exec::set_error_t>;
     inline constexpr just_error_t just_error{};
 
-    struct just_stopped_t {
-        [[nodiscard]] constexpr auto operator()() const noexcept -> just_sender<set_stopped_t> {
-            return {};
+    template<>
+    struct just_tag_t<exec::set_stopped_t> {
+        [[nodiscard]] constexpr decltype(auto) operator()() const {
+            return details::make_sender(*this, details::product_type{});
         }
     };
+    using just_stopped_t = just_tag_t<exec::set_stopped_t>;
     inline constexpr just_stopped_t just_stopped{};
 }
 
