0b941b7c641fa40f6dac49d3b870592be664677c
Molla7605 <jyc760500@gmail.com>
2026-02-24 03:19:23 +0900

    Add emplace_from

---
+++ include/exec/details/emplace_from.hpp
@@ -0,0 +1,24 @@
+#ifndef EXEC_DETAILS_EMPLACE_FROM_HPP
+#define EXEC_DETAILS_EMPLACE_FROM_HPP
+
+#include <concepts>
+#include <type_traits>
+
+namespace exec::details {
+    template<std::invocable InvocableT>
+    struct emplace_from {
+        using result_t = std::invoke_result_t<InvocableT>;
+
+        InvocableT invocable;
+
+        [[nodiscard]] constexpr operator result_t() && noexcept(std::is_nothrow_invocable_v<InvocableT>) {
+            return std::invoke(std::move(invocable));
+        }
+
+        [[nodiscard]] constexpr result_t operator()() && noexcept(std::is_nothrow_invocable_v<InvocableT>) {
+            return std::invoke(std::move(invocable));
+        }
+    };
+}
+
+#endif // !EXEC_DETAILS_EMPLACE_FROM_HPP
