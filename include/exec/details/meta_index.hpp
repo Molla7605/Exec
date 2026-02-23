e85c6f89c73e738e598e8b8ec05cc6041af5a83e
Molla7605 <jyc760500@gmail.com>
2026-02-24 03:23:41 +0900

    Use type_holder and standard library instead of type-specific containers

--- include/exec/details/meta_index.hpp
+++ include/exec/details/meta_index.hpp
@@ -1,45 +1,42 @@
 #ifndef EXEC_DETAILS_META_INDEX_HPP
 #define EXEC_DETAILS_META_INDEX_HPP
 
+#include "exec/details/type_holder.hpp"
 #include "exec/details/type_list.hpp"
 
 #include <cstddef>
+#include <type_traits>
 
 namespace exec::details {
-    namespace _meta_index {
-        template<typename...>
-        struct container {};
-
-        template<typename SizeT, SizeT INDEX>
-        struct index {
-            static constexpr SizeT value = INDEX;
-        };
-    }
-
     template<typename...>
     struct meta_index {};
 
+    template<typename SizeT, SizeT TARGET_INDEX, SizeT INDEX>
+    struct meta_index<std::integral_constant<SizeT, TARGET_INDEX>, std::integral_constant<SizeT, INDEX>> {
+        static_assert("Index out of ranged.");
+    };
+
     template<typename SizeT, SizeT TARGET_INDEX, SizeT INDEX, typename CurrentT, typename... CandidateTs>
     requires (TARGET_INDEX == INDEX)
-    struct meta_index<_meta_index::index<SizeT, TARGET_INDEX>, _meta_index::index<SizeT, INDEX>, CurrentT, CandidateTs...> {
+    struct meta_index<std::integral_constant<SizeT, TARGET_INDEX>, std::integral_constant<SizeT, INDEX>, CurrentT, CandidateTs...> {
         using type = CurrentT;
     };
 
     template<typename SizeT, SizeT TARGET_INDEX, SizeT INDEX, typename CurrentT, typename... CandidateTs>
-    struct meta_index<_meta_index::index<SizeT, TARGET_INDEX>, _meta_index::index<SizeT, INDEX>, CurrentT, CandidateTs...> {
-        using type = meta_index<_meta_index::index<SizeT, TARGET_INDEX>, _meta_index::index<SizeT, INDEX + 1>, CandidateTs...>::type;
+    struct meta_index<std::integral_constant<SizeT, TARGET_INDEX>, std::integral_constant<SizeT, INDEX>, CurrentT, CandidateTs...> {
+        using type = meta_index<std::integral_constant<SizeT, TARGET_INDEX>, std::integral_constant<SizeT, INDEX + 1>, CandidateTs...>::type;
     };
 
     template<template<typename...> typename TypeListT, typename... ElementTs, typename SizeT, SizeT TARGET_INDEX>
-    struct meta_index<TypeListT<ElementTs...>, _meta_index::index<SizeT, TARGET_INDEX>> {
-        using type = meta_index<_meta_index::index<SizeT, TARGET_INDEX>, _meta_index::index<SizeT, 0>, ElementTs...>::type;
+    struct meta_index<TypeListT<ElementTs...>, std::integral_constant<SizeT, TARGET_INDEX>> {
+        using type = meta_index<std::integral_constant<SizeT, TARGET_INDEX>, std::integral_constant<SizeT, 0>, ElementTs...>::type;
     };
 
     template<size_t INDEX, valid_type_list ListT>
-    using meta_index_of_t = meta_index<ListT, _meta_index::index<size_t, INDEX>>::type;
+    using meta_index_of_t = meta_index<ListT, std::integral_constant<size_t, INDEX>>::type;
 
     template<size_t INDEX, typename... Ts>
-    using meta_index_t = meta_index_of_t<INDEX, _meta_index::container<Ts...>>;
+    using meta_index_t = meta_index_of_t<INDEX, type_holder<Ts...>>;
 }
 
 #endif // !EXEC_DETAILS_META_INDEX_HPP
--- include/exec/details/unique_template.hpp
+++ include/exec/details/unique_template.hpp
@@ -3,44 +3,40 @@
 
 #include "exec/details/meta_append.hpp"
 #include "exec/details/meta_reverse.hpp"
+#include "exec/details/type_holder.hpp"
 
 #include <type_traits>
 
 namespace exec::details {
-    namespace _unique_template {
-        template<typename...>
-        struct candidates {};
-    }
-
     template<valid_type_list, typename...>
     struct unique_template {};
 
     template<typename ListT>
-    struct unique_template<ListT, _unique_template::candidates<>> {
+    struct unique_template<ListT, type_holder<>> {
         using type = ListT;
     };
 
     template<typename ListT, typename CurrentT>
-    struct unique_template<ListT, _unique_template::candidates<CurrentT>> {
+    struct unique_template<ListT, type_holder<CurrentT>> {
         using type = meta_append_front_t<ListT, CurrentT>;
     };
 
     template<typename ListT, typename... CandidateTs, typename CurrentT>
     requires (std::is_same_v<CurrentT, CandidateTs> || ... || false)
-    struct unique_template<ListT, _unique_template::candidates<CurrentT, CandidateTs...>> {
-        using type = unique_template<ListT, _unique_template::candidates<CandidateTs...>>::type;
+    struct unique_template<ListT, type_holder<CurrentT, CandidateTs...>> {
+        using type = unique_template<ListT, type_holder<CandidateTs...>>::type;
     };
 
     template<typename ListT, typename... CandidateTs, typename CurrentT>
-    struct unique_template<ListT, _unique_template::candidates<CurrentT, CandidateTs...>> {
-        using type = unique_template<meta_append_front_t<ListT, CurrentT>, _unique_template::candidates<CandidateTs...>>::type;
+    struct unique_template<ListT, type_holder<CurrentT, CandidateTs...>> {
+        using type = unique_template<meta_append_front_t<ListT, CurrentT>, type_holder<CandidateTs...>>::type;
     };
 
     template<template<typename...> typename TypeListT, typename... CandidateTs>
-    using unique_template_t = unique_template<TypeListT<>, meta_reverse_t<_unique_template::candidates<CandidateTs...>>>::type;
+    using unique_template_t = unique_template<TypeListT<>, meta_reverse_t<type_holder<CandidateTs...>>>::type;
 
     template<valid_type_list T>
-    using meta_unique_t = unique_template<empty_list_of_t<T>, typename elements_of<meta_reverse_t<T>>::template apply<_unique_template::candidates>>::type;
+    using meta_unique_t = unique_template<empty_list_of_t<T>, typename elements_of<meta_reverse_t<T>>::template apply<type_holder>>::type;
 }
 
 #endif // !EXEC_DETAILS_UNIQUE_TEMPLATE_HPP
