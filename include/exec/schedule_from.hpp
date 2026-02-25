9f12547470b52a7ad6d0804b911543133c8a7d09
Molla7605 <jyc760500@gmail.com>
2026-02-25 22:56:17 +0900

    Refactor schedule_from_t to use basic_sender

--- include/exec/schedule_from.hpp
+++ include/exec/schedule_from.hpp
@@ -1,6 +1,7 @@
 #ifndef EXEC_SCHEDULE_FROM_HPP
 #define EXEC_SCHEDULE_FROM_HPP
 
+#include "exec/completions.hpp"
 #include "exec/completion_signatures.hpp"
 #include "exec/env.hpp"
 #include "exec/operation_state.hpp"
@@ -9,186 +10,189 @@
 #include "exec/sender.hpp"
 #include "exec/transform_completion_signatures.hpp"
 
-#include "exec/details/as_tuple.hpp"
+#include "exec/details/basic_sender.hpp"
+#include "exec/details/conditional_meta_apply.hpp"
 #include "exec/details/decayed_tuple.hpp"
+#include "exec/details/dummy_receiver.hpp"
 #include "exec/details/forward_env.hpp"
+#include "exec/details/is_nothrow_signatures.hpp"
+#include "exec/details/join_env.hpp"
+#include "exec/details/meta_bind.hpp"
+#include "exec/details/meta_index.hpp"
+#include "exec/details/meta_merge.hpp"
+#include "exec/details/sched_attrs.hpp"
+#include "exec/details/signature_info.hpp"
+#include "exec/details/type_holder.hpp"
 #include "exec/details/type_list.hpp"
 
+#include <concepts>
 #include <exception>
+#include <print>
 #include <tuple>
 #include <type_traits>
 #include <utility>
 #include <variant>
 
 namespace exec {
-    template<typename OpT>
-    struct schedule_from_receiver1 {
-        using receiver_concept = receiver_t;
+    struct schedule_from_t;
 
-        OpT* op;
+    template<>
+    struct details::impls_for<schedule_from_t> : default_impls {
+        template<typename SigT>
+        using as_tuple_t =
+            signature_args_of<completion_signatures<SigT>>::template apply<
+                meta_bind_front<decayed_tuple,
+                                completion_tag_of_t<completion_signatures<SigT>>>::template type>;
 
-        template<typename... Ts>
-        void set_value(Ts&&... values) && noexcept {
-            op->template transfer_context<set_value_t>(std::forward<decltype(values)>(values)...);
-        }
+        template<typename... SigTs>
+        using as_variant_t = std::variant<std::monostate, as_tuple_t<SigTs>...>;
 
-        template<typename T>
-        void set_error(T&& value) && noexcept {
-            op->template transfer_context<set_error_t>(std::forward<decltype(value)>(value));
-        }
+        template<typename... ArgTs>
+        struct nothrow_movable {
+            static constexpr bool value =
+                []() consteval {
+                    using tuple_t =
+                        std::tuple<std::bool_constant<std::is_nothrow_constructible_v<std::decay_t<ArgTs>, ArgTs>>...>;
 
-        void set_stopped() && noexcept {
-            op->template transfer_context<set_stopped_t>();
-        }
+                    return std::apply([]<typename... Ts>(Ts&&... values) noexcept {
+                        return (values && ... && true);
+                    }, tuple_t{});
+                }();
+        };
 
-        [[nodiscard]] constexpr details::forward_env_of_t<typename OpT::receiver_t> get_env() const noexcept {
-            return details::forward_env(exec::get_env(op->receiver));
-        }
-    };
+        template<typename StateT, typename ReceiverT>
+        struct second_receiver {
+            using receiver_concept = exec::receiver_t;
 
-    template<typename OpT>
-    struct schedule_from_receiver2 {
-        using receiver_concept = receiver_t;
+            StateT* state;
 
-        OpT* op;
+            void set_value() && noexcept {
+                std::visit(
+                    [this]<typename TupleT>(TupleT& tuple) noexcept {
+                        if constexpr (!std::same_as<std::monostate, TupleT>) {
+                            std::apply(
+                                [this]<typename TagT, typename... Ts>(TagT& tag, Ts&... values) noexcept {
+                                    tag(std::move(state->receiver), std::move(values)...);
+                                },
+                                tuple
+                            );
+                        }
+                    },
+                    state->results
+                );
+            }
 
-        void set_value() && noexcept {
-            op->complete();
-        }
+            template<typename T>
+            void set_error(T&& value) && noexcept {
+                exec::set_error(std::move(state->receiver), std::forward<T>(value));
+            }
 
-        template<typename T>
-        void set_error(T&& value) && noexcept {
-            op->template passthrough<set_error_t>(std::forward<decltype(value)>(value));
-        }
+            void set_stopped() && noexcept {
+                exec::set_stopped(std::move(state->receiver));
+            }
 
-        void set_stopped() && noexcept {
-            op->template passthrough<set_stopped_t>();
-        }
+            [[nodiscard]] constexpr forward_env_of_t<ReceiverT> get_env() const noexcept {
+                return forward_env(exec::get_env(state->receiver));
+            }
+        };
 
-        [[nodiscard]] constexpr details::forward_env_of_t<typename OpT::receiver_t> get_env() const noexcept {
-            return details::forward_env(exec::get_env(op->receiver));
-        }
-    };
+        static constexpr auto get_attrs =
+            []<typename SchedulerT, typename ChildT>(const SchedulerT& scheduler, const ChildT& child) noexcept {
+                return join_env(sched_attrs(scheduler), forward_env(exec::get_env(child)));
+            };
 
-    template<typename SchedulerT, typename SenderT, typename ReceiverT>
-    struct schedule_from_operation_state {
-        using operation_state_concept = operation_state_t;
+        static constexpr auto get_completion_signatures =
+            []<typename SenderT, typename EnvT>(SenderT&&, EnvT&&) noexcept {
+                using child_sender_t = decltype(std::declval<SenderT>().template get<2>());
+                using schedule_t = schedule_result_t<meta_index_of_t<1, std::decay_t<SenderT>>>;
+                using child_completion_signatures_t =
+                    conditional_meta_apply_t<
+                        sends_stopped<schedule_t, EnvT>,
+                        meta_bind_front<meta_merge_t, completion_signatures_of_t<child_sender_t, EnvT>>::template type,
+                        type_holder<completion_signatures<set_stopped_t()>>,
+                        type_holder<>
+                    >;
 
-        using receiver_t = ReceiverT;
+                constexpr bool nothrow = is_nothrow_signatures<nothrow_movable, child_completion_signatures_t>;
 
-        using first_op_t = connect_result_t<SenderT, schedule_from_receiver1<schedule_from_operation_state>>;
-        using second_op_t = connect_result_t<details::schedule_result_t<SchedulerT>, schedule_from_receiver2<schedule_from_operation_state>>;
-        using state_t = std::variant<SenderT, first_op_t, second_op_t>;
+                if constexpr (nothrow) {
+                    return child_completion_signatures_t{};
+                }
+                else {
+                    return meta_merge_t<child_completion_signatures_t,
+                                        completion_signatures<set_error_t(std::exception_ptr)>>{};
+                }
+            };
 
-        template<typename... Ts>
-        using arg_variant_t = std::variant<std::monostate, details::as_tuple_t<Ts>...>;
+        static constexpr auto get_state =
+            []<typename SenderT, typename ReceiverT>(SenderT&& sender, ReceiverT& receiver)
+                noexcept(std::is_nothrow_invocable_v<connect_t,
+                                                     schedule_result_t<meta_index_of_t<1, std::decay_t<SenderT>>>,
+                                                     dummy_receiver<env_of_t<ReceiverT>>>)
+                requires sender_in<child_of_t<SenderT>, env_of_t<ReceiverT>>
+            {
+                struct state {
+                    using impls = impls_for<schedule_from_t>;
+                    using receiver_t = impls::second_receiver<state, ReceiverT>;
+                    using scheduler_t = meta_index_of_t<1, std::decay_t<SenderT>>;
+                    using schedule_t = schedule_result_t<scheduler_t>;
+                    using op_t = connect_result_t<schedule_t, receiver_t>;
 
-        using args_t =
-            details::elements_of<completion_signatures_of_t<SenderT, env_of_t<ReceiverT>>>::template apply<arg_variant_t>;
+                    using child_completion_signatures_t = completion_signatures_of_t<SenderT, env_of_t<ReceiverT>>;
 
-        SchedulerT scheduler;
-        ReceiverT receiver;
-        state_t state;
-        args_t args;
+                    using variant_t =
+                        elements_of<child_completion_signatures_t>::template apply<as_variant_t>;
 
-        void complete() & noexcept {
-            std::visit(
-                [&]<typename TupleT>(TupleT& tuple) {
-                    if constexpr (!std::is_same_v<std::remove_cvref_t<TupleT>, std::monostate>) {
-                        std::apply(
-                            [&]<typename TagT, typename... Ts>(TagT& tag, Ts&... values) {
-                                tag(std::move(receiver), std::move(values)...);
-                            },
-                            tuple
-                        );
+                    ReceiverT& receiver;
+                    variant_t results;
+                    op_t op;
+
+                    explicit state(scheduler_t schd, ReceiverT& receiver)
+                        noexcept(noexcept(exec::connect(exec::schedule(schd), receiver_t{ nullptr }))) :
+                            receiver(receiver),
+                            op(exec::connect(exec::schedule(schd), receiver_t{ this })) {}
+                };
+
+                return std::forward<SenderT>(sender).apply(
+                    [&]<typename SchedulerT>(auto&&, SchedulerT&& scheduler, auto&&...)
+                        mutable noexcept(std::is_nothrow_constructible_v<state>) -> decltype(auto)
+                    {
+                        return state{ scheduler, receiver };
                     }
-                },
-                args
-            );
-        }
+                );
+            };
 
-        template<typename TagT>
-        void transfer_context(auto&&... values) & noexcept {
-            using result_t = details::decayed_tuple<TagT, decltype(values)...>;
+        static constexpr auto complete =
+            []<typename StateT, typename ReceiverT, typename TagT, typename... ArgTs>
+                (auto, StateT& state, ReceiverT& receiver, TagT, ArgTs&&... args) noexcept -> void
+            {
+                using result_t = decayed_tuple<TagT, ArgTs...>;
+                constexpr bool nothrow = std::is_nothrow_constructible_v<result_t, TagT, ArgTs...>;
 
-            if constexpr (details::has_completion_tag_v<TagT, completion_signatures_of_t<SenderT, env_of_t<ReceiverT>>>) {
                 try {
-                    args.template emplace<result_t>(TagT{}, std::forward<decltype(values)>(values)...);
+                    state.results.template emplace<result_t>(TagT{}, std::forward<ArgTs>(args)...);
                 }
                 catch (...) {
-                    exec::set_error(std::move(receiver), std::current_exception());
+                    if constexpr (!nothrow) {
+                        exec::set_error(std::move(receiver), std::current_exception());
+                    }
                 }
-            }
 
-            if (args.valueless_by_exception()) {
-                return;
-            }
-            if (args.index() == 0) {
-                return;
-            }
+                if (state.results.valueless_by_exception()) {
+                    return;
+                }
+                if (state.results.index() == 0) {
+                    return;
+                }
 
-            second_op_t* op = nullptr;
-            try {
-                op = std::addressof(state.template emplace<second_op_t>(exec::connect(exec::schedule(scheduler), schedule_from_receiver2{ this })));
-            }
-            catch (...) {
-                exec::set_error(std::move(receiver), std::current_exception());
-            }
-
-            if (op) {
-                exec::start(*op);
-            }
-        }
-
-        template<typename TagT>
-        void passthrough(auto&&... values) & noexcept {
-            TagT{}(std::move(receiver), std::forward<decltype(values)>(values)...);
-        }
-
-        void start() & noexcept {
-            first_op_t* op = nullptr;
-            try {
-                op = std::addressof(state.template emplace<first_op_t>(exec::connect(std::get<SenderT>(state), schedule_from_receiver1{ this })));
-            }
-            catch (...) {
-                exec::set_error(std::move(receiver), std::current_exception());
-            }
-
-            if (op) {
-                exec::start(*op);
-            }
-        }
-    };
-
-    template<typename SchedulerT, typename SenderT>
-    struct schedule_from_sender {
-        using sender_concept = exec::sender_t;
-
-        SchedulerT scheduler;
-        SenderT sender;
-
-        template<typename EnvT>
-        [[nodiscard]] constexpr auto get_completion_signatures(const EnvT&) const noexcept {
-            return completion_signatures_of_t<SenderT, EnvT>{};
-        }
-
-        template<typename Self, receiver ReceiverT>
-        [[nodiscard]] constexpr auto connect(this Self&& self, ReceiverT&& receiver) {
-            return schedule_from_operation_state<SchedulerT, SenderT, ReceiverT> {
-                self.scheduler,
-                std::forward<ReceiverT>(receiver),
-                { std::forward_like<Self>(self.sender) },
-                {}
+                exec::start(state.op);
             };
-        }
     };
 
     struct schedule_from_t {
         template<scheduler SchedulerT, sender SenderT>
         [[nodiscard]] constexpr auto operator()(SchedulerT&& scheduler, SenderT&& input) const {
-            return schedule_from_sender<std::decay_t<SchedulerT>, std::decay_t<SenderT>> {
-                std::forward<SchedulerT>(scheduler), std::forward<SenderT>(input)
-            };
+            return details::make_sender(*this, std::forward<SchedulerT>(scheduler), std::forward<SenderT>(input));
         }
     };
     inline constexpr schedule_from_t schedule_from{};
