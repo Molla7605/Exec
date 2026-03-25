abd0e1040ce9291e6bf2603d47bb4576ed1a43ef
jyc76 <jyc760500@gmail.com>
2026-03-26 03:30:31 +0900

    Implements counting_scope

--- include/exec/counting_scope.hpp
+++ include/exec/counting_scope.hpp
@@ -2,10 +2,12 @@
 #define EXEC_COUNTING_SCOPE_HPP
 
 #include "exec/sender.hpp"
+#include "exec/stop_token.hpp"
 
 #include "exec/details/association.hpp"
 #include "exec/details/counting_scope_state.hpp"
 #include "exec/details/scope_join.hpp"
+#include "exec/details/stop_when.hpp"
 
 namespace exec {
     class simple_counting_scope {
@@ -75,6 +77,80 @@ namespace exec {
         }
 
         details::counting_scope_state m_state;
+
+    };
+
+    class counting_scope {
+    public:
+        using assoc_t = details::association_t<counting_scope>;
+
+        class token {
+        public:
+            template<sender SenderT>
+            [[nodiscard]] sender auto wrap(SenderT&& sender) const
+                noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<SenderT>, SenderT>)
+            {
+                return details::stop_when(std::forward<SenderT>(sender), m_scope->m_stop_source.get_token());
+            }
+
+            [[nodiscard]] assoc_t try_associate() const noexcept {
+                return m_scope->try_associate();
+            }
+
+        private:
+            friend class counting_scope;
+
+            explicit token(counting_scope* scope) noexcept :
+                m_scope(scope) {}
+
+            counting_scope* m_scope;
+
+        };
+
+        static constexpr std::size_t max_associations = details::counting_scope_state::max_associations;
+
+        counting_scope() noexcept = default;
+
+        ~counting_scope() noexcept = default;
+
+        counting_scope(counting_scope&&) = delete;
+
+        [[nodiscard]] token get_token() noexcept {
+            return token{ this };
+        }
+
+        void close() noexcept {
+            m_state.close();
+        }
+
+        [[nodiscard]] sender auto join() noexcept {
+            return details::scope_join(this);
+        }
+
+        void request_stop() noexcept {
+            m_stop_source.request_stop();
+        }
+
+    private:
+        friend class token;
+        friend class details::impls_for<details::scope_join_t>;
+        friend struct details::association_t<counting_scope>;
+
+        [[nodiscard]] assoc_t try_associate() noexcept {
+            return m_state.try_associate() ? assoc_t{ this } : assoc_t{};
+        }
+
+        void disassociate() noexcept {
+            m_state.disassociate();
+        }
+
+        template<typename StateT>
+        [[nodiscard]] bool try_start_join(StateT& state) {
+            return m_state.try_start_join(state);
+        }
+
+        details::counting_scope_state m_state;
+        inplace_stop_source m_stop_source;
 
     };
 }
