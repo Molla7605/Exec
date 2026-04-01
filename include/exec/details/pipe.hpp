b092ba5c24f5723eac8242371e859661fcb182f2
Molla7605 <jyc760500@gmail.com>
2026-04-02 02:58:59 +0900

    details::pipe: Make the pipe connection correctly

--- include/exec/details/pipe.hpp
+++ include/exec/details/pipe.hpp
@@ -1,6 +1,7 @@
 #ifndef EXEC_DETAILS_PIPE_HPP
 #define EXEC_DETAILS_PIPE_HPP
 
+#include "exec/just.hpp"
 #include "exec/sender.hpp"
 
 #include "exec/details/product_type.hpp"
@@ -16,10 +17,51 @@ namespace exec {
 
 namespace exec::details {
     template<typename T>
-    concept pipeable = std::derived_from<T, sender_adapter_closure<T>>;
+    concept pipeable =
+        std::derived_from<std::remove_cvref_t<T>, sender_adapter_closure<std::remove_cvref_t<T>>> &&
+        std::constructible_from<std::remove_cvref_t<T>, T> &&
+        requires(T pipe) {
+            { pipe(just()) } -> sender;
+        };
 
-    template<pipeable... PipeableTs>
+    template<typename... PipableTs>
+    struct pipe;
+
+    template<typename>
+    struct is_pipe : std::false_type {};
+
+    template<typename... PipeableTs>
+    struct is_pipe<pipe<PipeableTs...>> : std::true_type {};
+
+    template<typename T>
+    concept not_pipe = pipeable<std::remove_cvref_t<T>> && !is_pipe<T>::value;
+
+    template<typename... PipeableTs>
     struct pipe : sender_adapter_closure<pipe<PipeableTs...>> {
+        template<pipeable... InnerTs>
+        [[nodiscard]] friend constexpr auto operator|(pipe&& self, pipe<InnerTs...>&& other) noexcept {
+            return pipe<InnerTs..., PipeableTs...>(
+                sender_adapter_closure<pipe<PipeableTs..., InnerTs...>>{},
+                merge_product_type(std::forward_like<decltype(self)>(self.adapters), std::forward_like<decltype(other)>(other.adapters))
+            );
+        }
+
+        template<not_pipe AdapterT>
+        [[nodiscard]] friend constexpr auto operator|(pipe&& self, AdapterT&& right) noexcept {
+            return pipe<std::remove_cvref_t<AdapterT>, PipeableTs...>(
+                sender_adapter_closure<meta_append_front_t<pipe, std::remove_cvref_t<AdapterT>>>{},
+                merge_product_type(product_type{ std::forward<AdapterT>(right) }, std::forward_like<decltype(self)>(self.adapters))
+            );
+        }
+
+        template<not_pipe AdapterT>
+        [[nodiscard]] friend constexpr auto operator|(AdapterT&& left, pipe&& self) noexcept {
+            return pipe<PipeableTs..., std::remove_cvref_t<AdapterT>>(
+                sender_adapter_closure<meta_append_back_t<pipe, std::remove_cvref_t<AdapterT>>>{},
+                merge_product_type(std::forward_like<decltype(self)>(self.adapters), product_type{ std::forward<AdapterT>(left) })
+            );
+        }
+
         template<sender SenderT, typename AdapterT>
         [[nodiscard]] static constexpr decltype(auto) compose(SenderT&& sender, AdapterT&& adapter) {
             return std::forward<AdapterT>(adapter)(std::forward<SenderT>(sender));
@@ -41,7 +83,7 @@ namespace exec::details {
     };
 
     template<typename... PipeableTs>
-    pipe(PipeableTs...) -> pipe<std::decay_t<PipeableTs>...>;
+    pipe(sender_adapter_closure<pipe<PipeableTs...>>, product_type<PipeableTs...>) -> pipe<std::decay_t<PipeableTs>...>;
 
     struct pipe_operator {
         template<sender SenderT, pipeable Self>
@@ -49,9 +91,12 @@ namespace exec::details {
             return std::forward<Self>(self)(std::forward<SenderT>(sender));
         }
 
-        template<pipeable LAdapterT, pipeable RAdapterT>
+        template<not_pipe LAdapterT, not_pipe RAdapterT>
         [[nodiscard]] friend constexpr auto operator|(LAdapterT&& left, RAdapterT&& right) noexcept {
-            return pipe{ {}, std::forward<LAdapterT>(left), std::forward<RAdapterT>(right) };
+            return pipe{
+                sender_adapter_closure<pipe<std::remove_cvref_t<RAdapterT>, std::remove_cvref_t<LAdapterT>>>{},
+                product_type{ std::forward<RAdapterT>(right), std::forward<LAdapterT>(left) }
+            };
         }
     };
 }
