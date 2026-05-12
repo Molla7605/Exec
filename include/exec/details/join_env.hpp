7f735c3958038b47050e8d14b9b7182f069bce3d
Molla7605 <jyc760500@gmail.com>
2026-05-13 03:06:55 +0900

    join_env: Refactor to use merge_env instead of inheritance

--- include/exec/details/join_env.hpp
+++ include/exec/details/join_env.hpp
@@ -1,57 +1,64 @@
 #ifndef EXEC_DETAILS_JOIN_ENV_HPP
 #define EXEC_DETAILS_JOIN_ENV_HPP
 
-#include "exec/queryable.hpp"
+#include "exec/env.hpp"
 
-#include <concepts>
+#include "exec/details/env_traits.hpp"
+#include "exec/details/merge_env.hpp"
+
 #include <utility>
 
 namespace exec::details {
-    template<queryable LEnv, queryable REnv>
-    struct joined_env : LEnv, REnv {
-        template<typename QueryT>
-        [[nodiscard]] constexpr decltype(auto) query(QueryT) const noexcept {
-            if constexpr (requires { std::declval<const LEnv&>().query(QueryT{}); }) {
-                return static_cast<const LEnv&>(*this).query(QueryT{});
-            }
-            else {
-                return static_cast<const REnv&>(*this).query(QueryT{});
-            }
-        }
-    };
-
-    template<typename LEnv, typename REnv>
-    joined_env(LEnv, REnv) -> joined_env<LEnv, REnv>;
-
-    template<typename LEnv, typename REnv>
-    requires (!(std::derived_from<LEnv, empty_env> || std::derived_from<REnv, empty_env>) &&
-              (std::derived_from<LEnv, REnv> || std::derived_from<REnv, LEnv>))
-    [[nodiscard]] constexpr decltype(auto) join_env(LEnv&& l_env, REnv&& r_env) noexcept {
-        if constexpr (valid_forwarding_env<LEnv>) {
-            return std::forward<LEnv>(l_env);
-        }
-        else {
-            return std::forward<REnv>(r_env);
-        }
-    }
-
-    template<typename LEnv, typename REnv>
-    requires std::derived_from<LEnv, empty_env>
-    [[nodiscard]] constexpr decltype(auto) join_env(LEnv&&, REnv&& r_env) noexcept {
-        return std::forward<REnv>(r_env);
-    }
-
-    template<typename LEnv, typename REnv>
-    requires std::derived_from<REnv, empty_env>
-    [[nodiscard]] constexpr decltype(auto) join_env(LEnv&& l_env, REnv&&) noexcept {
-        return std::forward<LEnv>(l_env);
-    }
-
-    template<typename LEnv, typename REnv>
-    [[nodiscard]] constexpr joined_env<std::decay_t<LEnv>, std::decay_t<REnv>> join_env(LEnv&& l_env, REnv&& r_env)
-        noexcept(std::is_nothrow_constructible_v<joined_env<std::decay_t<LEnv>, std::decay_t<REnv>>, LEnv, REnv>)
+    template<valid_env LhsT, not_env RhsT>
+    requires (!valid_empty_env<LhsT>)
+    [[nodiscard]] constexpr auto join_env(LhsT&& lhs, RhsT&& rhs)
+        noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<LhsT>, LhsT> &&
+                 std::is_nothrow_constructible_v<std::remove_cvref_t<RhsT>, RhsT>)
     {
-        return { std::forward<LEnv>(l_env), std::forward<REnv>(r_env) };
+        return std::forward<LhsT>(lhs).apply([&]<typename... Ts>(Ts&&... props) {
+            using env_t = meta_append_back_t<std::remove_cvref_t<LhsT>, std::remove_cvref_t<RhsT>>;
+
+            return env_t{ std::forward<Ts>(props)..., std::forward<RhsT>(rhs) };
+        });
+    }
+
+    template<not_env LhsT, valid_env RhsT>
+    requires (!valid_empty_env<RhsT>)
+    [[nodiscard]] constexpr auto join_env(LhsT&& lhs, RhsT&& rhs)
+        noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<LhsT>, LhsT> &&
+                 std::is_nothrow_constructible_v<std::remove_cvref_t<RhsT>, RhsT>)
+    {
+        return std::forward<RhsT>(rhs).apply([&]<typename... Ts>(Ts&&... props) {
+            using env_t = meta_append_front_t<std::remove_cvref_t<RhsT>, std::remove_cvref_t<LhsT>>;
+
+            return env_t{ std::forward<LhsT>(lhs), std::forward<Ts>(props)... };
+        });
+    }
+
+    template<valid_empty_env LhsT, typename RhsT>
+    [[nodiscard]] constexpr decltype(auto) join_env(LhsT&&, RhsT&& rhs) noexcept {
+        return std::forward<RhsT>(rhs);
+    }
+
+    template<typename LhsT, valid_empty_env RhsT>
+    [[nodiscard]] constexpr decltype(auto) join_env(LhsT&& lhs, RhsT&&) noexcept {
+        return std::forward<LhsT>(lhs);
+    }
+
+    template<valid_env LhsT, valid_env RhsT>
+    [[nodiscard]] constexpr auto join_env(LhsT&& lhs, RhsT&& rhs)
+        noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<LhsT>, RhsT> &&
+                 std::is_nothrow_constructible_v<std::remove_cvref_t<RhsT>, RhsT>)
+    {
+        return merge_env(std::forward<LhsT>(lhs), std::forward<RhsT>(rhs));
+    }
+
+    template<typename LhsT, typename RhsT>
+    [[nodiscard]] constexpr auto join_env(LhsT&& lhs, RhsT&& rhs)
+        noexcept(std::is_nothrow_constructible_v<std::remove_cvref_t<LhsT>, RhsT> &&
+                 std::is_nothrow_constructible_v<std::remove_cvref_t<RhsT>, RhsT>)
+    {
+        return env{ std::forward<LhsT>(lhs), std::forward<RhsT>(rhs) };
     }
 }
 
